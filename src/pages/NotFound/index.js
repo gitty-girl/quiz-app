a41c4bd9b4ee6b4677e0c247a664fdf70a0cf9d7
gitty-girl <mariam.tchanturia1994@gmail.com>
2021-09-02 16:14:39 +0400

    fix typo

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,7 @@
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-  return <div className={styles.wrapper}>NotFound PAge</div>;
+  return <div className={styles.wrapper}>Not Found Page</div>;
 };
 
 export default NotFound;
